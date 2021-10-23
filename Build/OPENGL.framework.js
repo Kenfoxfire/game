2775e8c0d087d1ac26c224b327ebdf615980833c
Kenfoxfire <kennethsv10@gmail.com>
2021-10-22 22:01:33 -0600

    cambios

--- Build/OPENGL.framework.js
+++ Build/OPENGL.framework.js
@@ -1300,7 +1300,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2898320;
+STATICTOP = STATIC_BASE + 2898848;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3354,7 +3354,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2898320;
+var STATIC_BUMP = 2898848;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
